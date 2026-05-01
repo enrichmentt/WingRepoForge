59a422b1874addacd312322a1b7a8e5f5a547bc5
Vladimir Pekshev <enrichment.off@gmail.com>
2026-05-01 22:43:55 +0300

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# WingRepoForge 🚀
+Приватный REST-сервер репозитория для Windows Package Manager (winget), написанный на .NET.
---
+++ src/Api/Controllers/InformationController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Models.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WingRepoForge.Core.Models.Responses;
+
+namespace WingRepoForge.Api.Controllers
+{
+    [Route("[controller]")]
+    public class InformationController : Controller
+    {
+        private readonly ILogger<InformationController> _logger;
+
+        public InformationController(ILogger<InformationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("information")]
+        public ActionResult<ServerInformationResponse> GetInformation()
+        {
+            var response = new ServerInformationResponse(
+                SourceIdentifier: "WingRepoForge",
+                ServerVersion: "1.0.0",
+                ProtocolVersion: "1.0.0"
+            );
+
+            return Ok(response);
+        }
+
+        [HttpGet("manifestSearch")]
+        public ActionResult<ManifestSearchCapabilityResponse> GetSearchCapabilities()
+        {
+            var response = new ManifestSearchCapabilityResponse(
+                SupportedQueryMethods: new[] { "CaseInsensitiveSubstring" }
+            );
+
+            return Ok(response);
+        }
+    }
+}
